ac42c00e604600a8472073135900d3f54bcec4ab
ntk73 <nick@uni-plovdiv.net>
2016-09-16 15:34:01 +0300

    Added file StructureSetUtils

---
+++ src/main/java/pu/helpers/StructureSetUtils.java
@@ -0,0 +1,12 @@
+package pu.helpers;
+
+public class StructureSetUtils 
+{
+	public static int getNumberOfDublicatedNotations(String fileName1, String fileName2) throws Exception
+	{
+		int n = 0;
+		//TODO
+		
+		return n;
+	}
+}
