523a0c8ab2259ab5128a19b4d95b9dc66d73bd68
ntk73 <nick@uni-plovdiv.net>
2016-10-02 23:14:35 +0300

    Implemented conversion from cartesian to spherical coordinates and vice versa.

--- src/main/java/pu/qm/overlapint/CoordinateUtils.java
+++ src/main/java/pu/qm/overlapint/CoordinateUtils.java
@@ -5,12 +5,43 @@ public class CoordinateUtils
 	
 	public static double[] sphericalToCartesian(double r, double theta, double phi)
 	{
-		return null;
+		double c[] = new double[3];
+		//x
+		c[0] = r * Math.sin(theta) * Math.cos(phi);
+		//y
+		c[1] = r * Math.sin(theta) * Math.sin(phi);
+		//z
+		c[2] = r * Math.cos(theta);
+						
+		return c;
 	}
 	
 	public static double[] cartesianToSpherical(double x, double y, double z)
 	{
-		return null;
+		double c[] = new double[3];
+		double r = Math.sqrt(x*x + y*y + z*z);
+		
+		if (r == 0)
+		{	
+			c[0] = 0;
+			c[1] = 0;
+			c[2] = 0;
+		}
+		else
+		{
+			c[0] = r;
+			
+			//theta
+			c[1] = Math.acos(z/r);
+			
+			//phi
+			if ((x == 0) && (y == 0))
+				c[2] = 0;
+			else
+				c[2] = Math.atan2(y, x);
+		}
+		
+		return c;
 	}
 	
 }
