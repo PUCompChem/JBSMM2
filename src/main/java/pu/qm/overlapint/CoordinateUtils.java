461575046bba1ad8c9c4dd5fe424222c0c2abd75
ntk73 <nick@uni-plovdiv.net>
2016-10-02 22:19:25 +0300

    Implemented basic functionality in class OverlapIntegral

--- src/main/java/pu/qm/overlapint/CoordinateUtils.java
+++ src/main/java/pu/qm/overlapint/CoordinateUtils.java
@@ -2,5 +2,15 @@ package pu.qm.overlapint;
 
 public class CoordinateUtils 
 {
-
+	
+	public static double[] sphericalToCartesian(double r, double theta, double phi)
+	{
+		return null;
+	}
+	
+	public static double[] cartesianToSpherical(double x, double y, double z)
+	{
+		return null;
+	}
+	
 }
--- src/main/java/pu/qm/overlapint/OverlapIntegral.java
+++ src/main/java/pu/qm/overlapint/OverlapIntegral.java
@@ -1,14 +1,57 @@
 package pu.qm.overlapint;
 
+import pu.qm.wavefunction.IWaveFunction;
+
 public class OverlapIntegral 
 {	
 	public double R = 3.0;
 	public double dx = 0.001;
-	public double dy = 0.001;
-	public double dz = 0.001;
+	public double calcRadius = 100;
 	
-	//TODO describe orbital orientation
+	public double x1, y1, z1;
+	public double x2, y2, z2;
+	public double cx, cy, cz;
 	
+	//TODO describe orbital orientation ?
+	public IWaveFunction wave1 = null;
+	public IWaveFunction wave2 = null;
 	
+	public void setWorkPoints()
+	{
+		x1 = 0;
+		y1 = 0;
+		z1 = 0;
+		
+		x2 = R;
+		y2 = 0;
+		z2 = 0;
+		
+		cx = R / 2;
+		cy = 0;
+		cz = 0;
+	}
 	
+	public double calculate()
+	{
+		double x,y,z;
+		double sum = 0.0;
+		int n = (int) (calcRadius / dx);
+		
+		for (int i = -n; i <= n; i++)
+			for (int j = -n; j <= n; j++)
+				for (int k = -n; k <= n; k++)
+				{
+					x = cx + i * dx;
+					y = cy + j * dx;
+					z = cz + k * dx;
+					
+					//Calculate relative coordinates according to point 1 and point 2
+					double sc1[] = CoordinateUtils.cartesianToSpherical(x-x1, y-y1, z-z1);
+					double sc2[] = CoordinateUtils.cartesianToSpherical(x-x2, y-y2, z-z2);
+					
+					sum += wave1.F(sc1[0], sc1[1], sc1[2]) * wave2.F(sc2[0], sc2[1], sc2[2]);
+				}
+		
+		return sum;
+	}
 }
