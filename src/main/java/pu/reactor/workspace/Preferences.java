4be9712e0e493ffca9aa87d86c98b406c80e73c5
Gogohero <nrggig@gmail.com>
2017-05-16 14:53:51 +0300

    Added CheckBox to Preferences jsonParsing

--- src/main/java/pu/reactor/workspace/Preferences.java
+++ src/main/java/pu/reactor/workspace/Preferences.java
@@ -1,46 +1,10 @@
 package pu.reactor.workspace;
 
-import pu.reactor.workspace.gui.PreferencesWindow;
-
-public class Preferences 
-{
-	public static String reactionDBPath = null;
-	public static String startingMaterialsPath = null;
-	
-	public PreferencesWindow createPreferencesWindow()
-	{
-		//TODO
-		
-		return null;
-	}
-	
-	public String toJsonString()
-	{	
-		String endLine = "\n";
-		StringBuffer sb = new StringBuffer();
-		int nFields = 0;
-		sb.append("{" + endLine);		
-		
-		if (reactionDBPath != null)
-		{
-			sb.append("\t\"REACTION_DB_PATH\" : " + "\"" + reactionDBPath + "\"");
-			nFields++;
-		}
-		
-		if (reactionDBPath != null)
-		{
-			if (nFields > 0)
-				sb.append("," + endLine);
-			else
-				sb.append(endLine);
+public class Preference {
+	public boolean BoolValueCheckbox3 = false;
+		public Preference(){
 			
-			sb.append("\t\"STARTING_MATERIALS_PATH\" : " + "\"" + startingMaterialsPath + "\"");
-			nFields++;
 		}
-		
-		sb.append(endLine);
-		
-		sb.append("}" + endLine);
-		return sb.toString();
-	}
+	public String ToString(){
+			return Boolean.toString(BoolValueCheckbox3);}
 }
