cf86ec3fe17e8457dad5d048433b0deaea560a7e
Gogohero <nrggig@gmail.com>
2017-05-18 15:20:16 +0300

    Added writing Jackson functionality in Preferences class

--- src/main/java/pu/reactor/workspace/Preferences.java
+++ src/main/java/pu/reactor/workspace/Preferences.java
@@ -1,19 +1,46 @@
 package pu.reactor.workspace;
 
+import pu.reactor.workspace.gui.PreferencesWindow;
+
 public class Preferences 
 {
 	public static String reactionDBPath = null;
 	public static String startingMaterialsPath = null;
-
-	public boolean BoolValueCheckbox3 = false;
 	
-	public Preferences()
+	public PreferencesWindow createPreferencesWindow()
 	{
-
+		//TODO
+		
+		return null;
 	}
-	public String toJsonString(){
-		return Boolean.toString(BoolValueCheckbox3);
+	
+	public String toJsonString()
+	{	
+		String endLine = "\n";
+		StringBuffer sb = new StringBuffer();
+		int nFields = 0;
+		sb.append("{" + endLine);		
+		
+		if (reactionDBPath != null)
+		{
+			sb.append("\t\"REACTION_DB_PATH\" : " + "\"" + reactionDBPath + "\"");
+			nFields++;
+		}
+		
+		if (reactionDBPath != null)
+		{
+			if (nFields > 0)
+				sb.append("," + endLine);
+			else
+				sb.append(endLine);
+			
+			sb.append("\t\"STARTING_MATERIALS_PATH\" : " + "\"" + startingMaterialsPath + "\"");
+			nFields++;
+		}
+		
+		sb.append(endLine);
+		
+		sb.append("}" + endLine);
+		return sb.toString();
 	}
-
-
 }
