982bc4fa3377532de8f4dcc1e47b197ba69a7ea8
ntk73 <nick@uni-plovdiv.net>
2017-07-07 15:07:09 +0300

    Implemented basic functionality in class ColorScheme

--- src/main/java/pu/filtering/ColorScheme.java
+++ src/main/java/pu/filtering/ColorScheme.java
@@ -1,14 +1,53 @@
 package pu.filtering;
 
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class ColorScheme 
 {
-	//TODO mapping between codes and color
+	//mapping between codes and color
+	public Map<Integer, Color> codeColors = new HashMap<Integer, Color>();
 	
-	public static ColorScheme generateColorScheme(Set<Integer> code)
-	{
-		//TODO
+	public static ColorScheme getPredefinedColorScheme(Set<Integer> codes)
+	{	
+		List<Color> colors = null;
+		
+		switch (codes.size())
+		{
+		case 2:
+			colors = getPredefined2Colors();
+			break;
+		}
+		
+		
+		if (colors != null)
+		{	
+			ColorScheme colSch= new ColorScheme(); 
+			int i = 0;
+			Iterator<Integer> it = codes.iterator();
+			while (it.hasNext())
+			{	
+				Integer code = it.next();
+				colSch.codeColors.put(code, colors.get(i));
+				i++;
+			}
+			return colSch;
+		}
+
 		return null;
+	}
+	
+	
+	public static List<Color> getPredefined2Colors()
+	{
+		List<Color> colors = new ArrayList<Color>();
+		colors.add(Color.RED);
+		colors.add(Color.GREEN);
+		return colors;
 	}
 }
