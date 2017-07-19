a1756e0fcc563a539a5963b8b5e5abdbe0b18d60
ntk73 <nick@uni-plovdiv.net>
2017-07-19 23:53:59 +0300

    Added Veretx parameters. Added new functionality in IGeometryAlgorithm interface

--- src/main/java/pu/gui/graph2d/IGeometryAlgorithm.java
+++ src/main/java/pu/gui/graph2d/IGeometryAlgorithm.java
@@ -4,6 +4,9 @@ public interface IGeometryAlgorithm
 {
 	public void setGraph(Graph2D graph);
 	public Graph2D getGraph();
+	public void setRoot(Vertex v);
+	public void addVertexChild(Vertex parent, Vertex child);
+	public void remoceVertexChild(Vertex parent, Vertex child);
 	
 	//TODO add geometry operations: zooming, scaling, adding/removinf nodes
 	
--- src/main/java/pu/gui/graph2d/VertexParameters.java
+++ src/main/java/pu/gui/graph2d/VertexParameters.java
@@ -2,5 +2,13 @@ package pu.gui.graph2d;
 
 public class VertexParameters 
 {
-
+	public static enum Shape {
+		RECTANGLE, OVAL, TRIANGLE, DIAMOND
+	}
+	
+	public int xpos = 0;
+	public int ypos = 0;
+	public int xsize = 0;
+	public int ysize = 0;
+	
 }
