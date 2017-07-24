42c8ede62c92787ef596629424844a94a061c4c1
Gogohero <nrggig@gmail.com>
2017-07-24 14:51:15 +0300

    Revert "Implemented new class VertexNode"
    This reverts commit 0399c3ac754bb4bd330437c5a86c395137f2e543.

--- src/main/java/pu/gui/graph2d/Graph2D.java
+++ src/main/java/pu/gui/graph2d/Graph2D.java
@@ -13,14 +13,13 @@ public class Graph2D
 	
 	protected List<Vertex> vertices = new ArrayList<Vertex>();
 	protected List<Edge> edges = new ArrayList<Edge>();
-	protected VertexNode treeRoot = null;
+	protected Vertex treeRoot = null;
 	
 	protected GraphType graphType = GraphType.TREE;
 	protected IGeometryAlgorithm algorithm = null;
 	protected EdgeFormat defaultEdgeFormat = null;
 	protected VertexFormat defaultVertexFormat = null;
 	
-	protected boolean FlagRegisterFullGraphInfoForTreeGraph = false;
 	
 	public Graph2D()
 	{
@@ -32,13 +31,15 @@ public class Graph2D
 		algorithm = new BasicTreeGeometryAlgorithm();
 	}
 	
-	public VertexNode getTreeRoot() {
+	public Vertex getTreeRoot() {
 		return treeRoot;
 	}
 
-	public void setTreeRoot(VertexNode treeRoot) {
+	public void setTreeRoot(Vertex treeRoot) {
 		this.treeRoot = treeRoot;
-	}	
+	}
+
+	
 	
 	public EdgeFormat getDefaultEdgeFormat() {
 		return defaultEdgeFormat;
@@ -73,10 +74,8 @@ public class Graph2D
 	}
 	
 	public void renderGraph() {
-		//TODO  ?? make IRenderer interface and implementations of it
+		//TODO
 	}
-	
-	//TODO add graph manipulation methods for vertices, edges and nodes
 	
 	
 }
