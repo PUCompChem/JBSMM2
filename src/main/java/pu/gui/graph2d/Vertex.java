662183343a5a7237f58dcd8114b12cc239b53a09
ntk73 <nick@uni-plovdiv.net>
2017-07-24 17:44:19 +0300

    Added enum VertexStatus

--- src/main/java/pu/gui/graph2d/Vertex.java
+++ src/main/java/pu/gui/graph2d/Vertex.java
@@ -2,9 +2,14 @@ package pu.gui.graph2d;
 
 public class Vertex 
 {
+	public static enum VertexStatus {
+		EXPANDED, CLOSED, EXPANDED_INACTIVE, CLOSED_INACTIVE, HIDDEN
+	}
+	
 	public VertexFormat format = null;
 	public VertexParameters params = null;
 	public VertexZone zone = null;
+	public VertexStatus status = VertexStatus.EXPANDED;
 	
 	public Object vertexObject = null;
 	
