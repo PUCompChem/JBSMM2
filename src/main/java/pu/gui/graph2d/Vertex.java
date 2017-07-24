98bd1ab7ff9cbdd15c4d72cb8a2f70f681ddcc60
ntk73 <nick@uni-plovdiv.net>
2017-07-24 17:10:53 +0300

    Removed redundant functionality from class Vertex (it has been moved to VertexNode)

--- src/main/java/pu/gui/graph2d/Vertex.java
+++ src/main/java/pu/gui/graph2d/Vertex.java
@@ -1,40 +1,11 @@
 package pu.gui.graph2d;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Vertex 
 {
 	public VertexFormat format = null;
 	public VertexParameters params = null;
 	public VertexZone zone = null;
-	public List<Vertex> childVertices = new ArrayList<Vertex>();
-	public List<Edge> childEdges = new ArrayList<Edge>();
 	
 	public Object vertexObject = null;
-		
-	public void addChild(Vertex v, Edge e)
-	{
-		childVertices.add(v);
-		childEdges.add(e);
-	}
 	
-	public void removeChild(Vertex v)
-	{
-		int index = childVertices.indexOf(v);
-		if (index != -1)
-		{
-			childVertices.remove(index);
-			childEdges.remove(index);
-		}
-	}
-	
-	public void removeChild(int index)
-	{	
-		if (index >= 0 && index < childVertices.size())
-		{
-			childVertices.remove(index);
-			childEdges.remove(index);
-		}
-	}
 }
