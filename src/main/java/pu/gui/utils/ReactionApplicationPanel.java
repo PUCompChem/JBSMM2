87a0e07dcb0819c856f4097c22ae480f68cbcd9e
ntk73 <nick@uni-plovdiv.net>
2017-12-06 18:48:37 +0200

    Implemented major functionality for reaction application

--- src/main/java/pu/gui/utils/ReactionApplicationPanel.java
+++ src/main/java/pu/gui/utils/ReactionApplicationPanel.java
@@ -14,11 +14,21 @@ import javax.swing.JTextField;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.EtchedBorder;
 
+import org.openscience.cdk.CDKConstants;
+import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IAtomContainerSet;
+import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
+import org.openscience.cdk.smiles.SmilesGenerator;
+import org.openscience.cdk.tools.CDKHydrogenAdder;
+import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
 import pu.gui.utils.chemtable.StructureTable;
+import ambit2.core.data.MoleculeTools;
+import ambit2.core.helper.CDKHueckelAromaticityDetector;
 import ambit2.smarts.SMIRKSManager;
+import ambit2.smarts.SMIRKSReaction;
 import ambit2.smarts.SmartsConst;
 import ambit2.smarts.SmartsHelper;
 import ambit2.smarts.SmartsConst.HandleHAtoms;
@@ -26,24 +36,31 @@ import ambit2.ui.Panel2D;
 
 public class ReactionApplicationPanel extends JPanel
 {
+	enum ReactionOperation {
+		APPLY, CombinedOverlappedPos, SingleCopyForEachPos
+	}
 	
 	private static final long serialVersionUID = 112332423349877L;
-	
+
 	//GUI components
 	JPanel targetPanel, matchPanel, reactionPanel, buttonsPanel;
 	JTextField smilesField, smirksField;
 	Panel2D panel2d;
 	StructureTable structureTable;
 	JButton applyButton;
-	
+
 	//Chem data
 	SMIRKSManager smrkMan = new SMIRKSManager(SilentChemObjectBuilder.getInstance());
+	IAtomContainer targetMol = null;
+	
+	ReactionOperation FlagReactionOperation = ReactionOperation.SingleCopyForEachPos;
+	
 	boolean FlagClearAromaticityBeforePreProcess = true;
-	//boolean FlagCheckAromaticityOnTargetPreProcess = true;
-	//boolean FlagTargetPreprocessing = false;
-	//boolean FlagExplicitHAtoms = false;  
-	//boolean FlagPrintAtomAttributes = false;
-	//boolean FlagPrintTransformationData = false;
+	boolean FlagCheckAromaticityOnTargetPreProcess = true;
+	boolean FlagTargetPreprocessing = false;
+	boolean FlagExplicitHAtoms = false;  
+	boolean FlagPrintAtomAttributes = false;
+	boolean FlagPrintTransformationData = false;
 
 	boolean FlagProductPreprocessing = true;
 	boolean FlagClearImplicitHAtomsBeforeProductPreProcess = false;
@@ -51,7 +68,7 @@ public class ReactionApplicationPanel extends JPanel
 	boolean FlagAddImplicitHAtomsOnProductPreProcess = false;
 	boolean FlagImplicitHToExplicitOnProductPreProcess = false;
 	boolean FlagExplicitHToImplicitOnProductPreProcess = false;
-	
+
 	boolean FlagApplyStereoTransformation = false;
 	boolean FlagHAtomsTransformation = false;
 	HandleHAtoms FlagHAtomsTransformationMode = HandleHAtoms.IMPLICIT; 
@@ -61,32 +78,32 @@ public class ReactionApplicationPanel extends JPanel
 	boolean FlagDoubleBondAromaticityNotSpecified = false;
 
 	SmartsConst.SSM_MODE FlagSSMode = SmartsConst.SSM_MODE.SSM_NON_OVERLAPPING;
-	//SmartsConst.SSM_MODE FlagSSModeForSingleCopyForEachPos = SmartsConst.SSM_MODE.SSM_NON_IDENTICAL;
-	
-	
+	SmartsConst.SSM_MODE FlagSSModeForSingleCopyForEachPos = SmartsConst.SSM_MODE.SSM_NON_IDENTICAL;
+
+
 	public ReactionApplicationPanel()
 	{
 		initGUI();
-		
+
 		//Setup initial demo chem data
 		try {
-			IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles("c1ccccc1");
-			panel2d.setAtomContainer(mol);
+			targetMol = SmartsHelper.getMoleculeFromSmiles("c1ccccc1");
+			panel2d.setAtomContainer(targetMol);
 		} catch (Exception e) {}
 		smilesField.setText("c1ccccc1");
 		smirksField.setText("[c:1][H]>>[c:1]O[H]");
-		
+
 		structureTable.addMoleculeAsString("CCCC");
 		structureTable.addMoleculeAsString("CCC(N)CO");
 		structureTable.addMoleculeAsString("CCCCC");
 		structureTable.addMoleculeAsString("CCCCCCC");
 		structureTable.addMoleculeAsString("CCCCNCCO");
 	}
-	
+
 	private void initGUI()
 	{	
 		this.setLayout(new BorderLayout());
-		
+
 		//Target panel
 		targetPanel = new JPanel(new BorderLayout());
 		this.add(targetPanel, BorderLayout.NORTH);	
@@ -100,31 +117,31 @@ public class ReactionApplicationPanel extends JPanel
 		panel2d = new Panel2D();
 		panel2d.setPreferredSize(new Dimension(150,150));
 		targetPanel.add(panel2d, BorderLayout.CENTER);
-		
+
 		//Reaction panel
 		reactionPanel = new JPanel(new BorderLayout());
 		this.add(reactionPanel, BorderLayout.CENTER);
 		//reactionPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
-		
+
 		JPanel smirksPanel = new JPanel(new FlowLayout());
 		JLabel smirksLabel = new JLabel("SMIRKS: ");
 		smirksField = new JTextField(30);
 		smirksPanel.add(smirksLabel);
 		smirksPanel.add(smirksField);
 		reactionPanel.add(smirksPanel, BorderLayout.NORTH);
-		
+
 		JPanel productsPanel = new JPanel(new BorderLayout());
 		JLabel productsLabel = new JLabel("Reaction products:");
 		productsPanel.add(productsLabel, BorderLayout.NORTH);
 		structureTable = new StructureTable(3);
 		productsPanel.add(structureTable, BorderLayout.CENTER);
 		reactionPanel.add(productsPanel, BorderLayout.CENTER);
-		
+
 		//Buttons panel
 		buttonsPanel = new JPanel(new FlowLayout());
 		this.add(buttonsPanel, BorderLayout.SOUTH);
 		buttonsPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
-		
+
 		applyButton = new JButton("Apply");
 		buttonsPanel.add(applyButton);
 		applyButton.addActionListener(new ActionListener() {
@@ -135,23 +152,27 @@ public class ReactionApplicationPanel extends JPanel
 		});
 
 	}
-	
+
 	private void handleApplyButton(ActionEvent e)
 	{
 		try {
-			IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles(smilesField.getText());
-			panel2d.setAtomContainer(mol);
+			targetMol = SmartsHelper.getMoleculeFromSmiles(smilesField.getText());
+			panel2d.setAtomContainer(targetMol);
 		} catch (Exception x) {
 			System.out.println("Incorrect input smiles: " + x.getMessage());
 		}
 		
-		
-		//smirksField.setText("[c:1][H]>>[c:1]O[H]");
-		
-		//System.out.println("---- apply ------");
+		try {
+			String smirks = smirksField.getText();
+			IAtomContainer target = targetMol.clone();
+			applySMIRKSReaction(smirks, target);
+		}	
+		catch (Exception x) {
+			System.out.println("Error while applying reaction: " + x.getMessage());
+		}
 	}
-	
-	private void applySMIRKSReaction() throws Exception
+
+	private void applySMIRKSReaction(String smirks, IAtomContainer target) throws Exception
 	{
 		smrkMan.setFlagSSMode(FlagSSMode);
 
@@ -170,6 +191,101 @@ public class ReactionApplicationPanel extends JPanel
 
 		smrkMan.getSmartsParser().mSupportSingleBondAromaticityNotSpecified = FlagSingleBondAromaticityNotSpecified;
 		smrkMan.getSmartsParser().mSupportDoubleBondAromaticityNotSpecified = FlagDoubleBondAromaticityNotSpecified;
+
+		SMIRKSReaction reaction = smrkMan.parse(smirks);
+		if (!smrkMan.getErrors().equals("")) {
+			System.out.println(smrkMan.getErrors());
+			return;
+		}
+
+		if (FlagTargetPreprocessing)
+			this.preProcess(target);
+		
+		switch (FlagReactionOperation) 
+		{
+		case APPLY:
+			boolean res = smrkMan.applyTransformation(target, reaction);
+
+			if (FlagPrintAtomAttributes) {
+				System.out.println("Product:");
+				System.out.println("Product atom attributes:\n"
+						+ SmartsHelper.getAtomsAttributes(target));
+				System.out.println("Product bond attributes:\n"
+						+ SmartsHelper.getBondAttributes(target));
+			}
+
+			System.out.println("    "
+					+ SmartsHelper.moleculeToSMILES(target, true));
+
+			String transformedSmiles = SmartsHelper.moleculeToSMILES(target,
+					true);
+
+			if (res)
+			{	
+				/*
+				System.out.println("Reaction application: " + targetSmiles
+						+ "  -->  " + transformedSmiles + "    abs. smiles res " + 
+						SmilesGenerator.absolute().create(target));
+				*/		
+			}	
+			else
+				System.out.println("Reaction not appicable!");
+			break;
+
+		case CombinedOverlappedPos:
+			IAtomContainerSet resSet = smrkMan
+			.applyTransformationWithCombinedOverlappedPos(target, null,
+					reaction);
+			if (resSet == null)
+				System.out.println("Reaction not appicable!");
+			else {
+				System.out
+				.println("Reaction application With Combined Overlapped Positions: ");
+				for (int i = 0; i < resSet.getAtomContainerCount(); i++)
+					System.out.println(SmartsHelper.moleculeToSMILES(
+							resSet.getAtomContainer(i), true));
+			}
+			break;
+
+		case SingleCopyForEachPos:
+			IAtomContainerSet resSet2 = smrkMan
+			.applyTransformationWithSingleCopyForEachPos(target, null,
+					reaction, FlagSSModeForSingleCopyForEachPos);
+			if (resSet2 == null)
+				System.out.println("Reaction not appicable!");
+			else {
+				System.out
+				.println("Reaction application With Single Copy For Each Position: ");
+				for (int i = 0; i < resSet2.getAtomContainerCount(); i++)
+					System.out.println(SmartsHelper.moleculeToSMILES(
+							resSet2.getAtomContainer(i), true));
+			}
+			break;
+		}
+
 	}
-	
+
+	public void preProcess(IAtomContainer mol) throws Exception
+	{
+		if (FlagClearAromaticityBeforePreProcess) {
+			for (IAtom atom : mol.atoms())
+				if (atom.getFlag(CDKConstants.ISAROMATIC))
+					atom.setFlag(CDKConstants.ISAROMATIC, false);
+			for (IBond bond : mol.bonds())
+				if (bond.getFlag(CDKConstants.ISAROMATIC))
+					bond.setFlag(CDKConstants.ISAROMATIC, false);
+		}
+
+		AtomContainerManipulator.percieveAtomTypesAndConfigureAtoms(mol);
+
+		CDKHydrogenAdder adder = CDKHydrogenAdder
+				.getInstance(SilentChemObjectBuilder.getInstance());
+		adder.addImplicitHydrogens(mol);
+		if (FlagExplicitHAtoms)
+			MoleculeTools.convertImplicitToExplicitHydrogens(mol);
+
+		if (FlagCheckAromaticityOnTargetPreProcess)	
+			CDKHueckelAromaticityDetector.detectAromaticity(mol);
+	}
+
 }
