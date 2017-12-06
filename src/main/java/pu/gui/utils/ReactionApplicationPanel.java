c34f492a49dab7521666240c1531a9965d418198
ntk73 <nick@uni-plovdiv.net>
2017-12-06 11:35:46 +0200

    Added Panel2D

--- src/main/java/pu/gui/utils/ReactionApplicationPanel.java
+++ src/main/java/pu/gui/utils/ReactionApplicationPanel.java
@@ -8,9 +8,18 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+import ambit2.smarts.SmartsHelper;
+import ambit2.ui.Panel2D;
+
 public class ReactionApplicationPanel extends JPanel
 {
+	
+	private static final long serialVersionUID = 112332423349877L;
+	
 	JPanel targetPanel, matchPanel, reactionPanel;
+	Panel2D panel2d;
 	
 	public ReactionApplicationPanel()
 	{
@@ -19,16 +28,29 @@ public class ReactionApplicationPanel extends JPanel
 	
 	private void initGUI()
 	{
-		setSize(new Dimension(400,600));
-		setLayout(new BorderLayout());
-		targetPanel = new JPanel(new FlowLayout());
-		reactionPanel = new JPanel(new FlowLayout());
-		add(targetPanel,BorderLayout.NORTH);
+		this.setSize(new Dimension(400,600));
+		this.setLayout(new BorderLayout());
+		
+		targetPanel = new JPanel(new BorderLayout());
+		reactionPanel = new JPanel(new BorderLayout());
+		this.add(targetPanel,BorderLayout.NORTH);
+		this.add(targetPanel,BorderLayout.SOUTH);
+		
+		JPanel smilesPanel = new JPanel(new FlowLayout());
 		JLabel smilesInputLabel = new JLabel("Molecule (Smiles/InChI):");
 		JTextField smilesField = new JTextField(20);
-		targetPanel.add(smilesInputLabel);
-		targetPanel.add(smilesField);
+		smilesPanel.add(smilesInputLabel);
+		smilesPanel.add(smilesField);
+		targetPanel.add(smilesPanel, BorderLayout.NORTH);
 		
+		panel2d = new Panel2D();
+		targetPanel.add(panel2d, BorderLayout.CENTER);
+		try {
+			IAtomContainer mol = SmartsHelper.getMoleculeFromSmiles("CCCc1ccccc1N");
+			panel2d.setAtomContainer(mol);
+		} catch (Exception e) {}
+
+
 	}
 	
 }
