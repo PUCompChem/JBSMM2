68a42ffee7569727bc0475af71af605b8abc3302
ntk73 <nick@uni-plovdiv.net>
2017-12-11 23:29:59 +0200

    Added basic fields and BroserMode enum

--- src/main/java/pu/reactor/workspace/gui/ReactionBrowser.java
+++ src/main/java/pu/reactor/workspace/gui/ReactionBrowser.java
@@ -4,18 +4,29 @@ import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
+import org.openscience.cdk.interfaces.IAtomContainer;
+
 import pu.gui.utils.chemtable.SmartChemTable;
 
 public class ReactionBrowser extends JFrame 
 {
-		
 	private static final long serialVersionUID = 7769828597142198311L;
 	
-	SmartChemTable chemTabl;
+	enum BrowserMode {
+		REACTION_ONLY, REACTION_APPLICATION
+	}
+	
+	BrowserMode mode = BrowserMode.REACTION_APPLICATION;
+	SmartChemTable chemTable = null;
+	IAtomContainer target = null;
+	String targetString = "";
+	
+	//Table columns config
+	boolean showRowNumber = true;
+	
 	
 	public ReactionBrowser() 
 	{
-
 		//this.setLayout(new BorderLayout());
 		initGUI();
 		setSize(new Dimension(400,600));
@@ -26,7 +37,45 @@ public class ReactionBrowser extends JFrame
 	private void initGUI() {
 		
 	}
-	
-	
+
+	public BrowserMode getMode() {
+		return mode;
+	}
+
+	public void setMode(BrowserMode mode) {
+		this.mode = mode;
+	}
+
+	public SmartChemTable getChemTable() {
+		return chemTable;
+	}
+
+	public void setChemTable(SmartChemTable chemTable) {
+		this.chemTable = chemTable;
+	}
+
+	public IAtomContainer getTarget() {
+		return target;
+	}
+
+	public void setTarget(IAtomContainer target) {
+		this.target = target;
+	}
+
+	public String getTargetString() {
+		return targetString;
+	}
+
+	public void setTargetString(String targetString) {
+		this.targetString = targetString;
+	}
+
+	public boolean isShowRowNumber() {
+		return showRowNumber;
+	}
+
+	public void setShowRowNumber(boolean showRowNumber) {
+		this.showRowNumber = showRowNumber;
+	}
 
 }
