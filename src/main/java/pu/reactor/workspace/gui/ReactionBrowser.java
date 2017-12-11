b47238f3faa016f2ee64edb285be8945c28c45e9
ntk73 <nick@uni-plovdiv.net>
2017-12-12 00:22:49 +0200

    Added browse items and table columns config

--- src/main/java/pu/reactor/workspace/gui/ReactionBrowser.java
+++ src/main/java/pu/reactor/workspace/gui/ReactionBrowser.java
@@ -1,11 +1,15 @@
 package pu.reactor.workspace.gui;
 
 import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 
 import org.openscience.cdk.interfaces.IAtomContainer;
 
+import ambit2.reactions.Reaction;
+import ambit2.reactions.ReactionDataBase;
 import pu.gui.utils.chemtable.SmartChemTable;
 
 public class ReactionBrowser extends JFrame 
@@ -16,14 +20,26 @@ public class ReactionBrowser extends JFrame
 		REACTION_ONLY, REACTION_APPLICATION
 	}
 	
+	class BrowseItem {
+		public Reaction reaction = null;
+	}
+	
 	BrowserMode mode = BrowserMode.REACTION_APPLICATION;
 	SmartChemTable chemTable = null;
 	IAtomContainer target = null;
 	String targetString = "";
+	ReactionDataBase reactionDB = null;
+	List<BrowseItem> browseItems = new ArrayList<BrowseItem>();
+	List<Integer> itemSelection = null;
 	
 	//Table columns config
 	boolean showRowNumber = true;
-	
+	boolean showReactionName = true;
+	boolean showReactionSmirks = true;
+	boolean showTarget = true;
+	boolean showProducts = true;
+	boolean showReactionScore = true;
+	boolean showReactionDepiction = false;
 	
 	public ReactionBrowser() 
 	{
@@ -34,9 +50,21 @@ public class ReactionBrowser extends JFrame
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	}
 	
-	private void initGUI() {
-		
+	private void initGUI() 
+	{
+		//TODO
 	}
+	
+	private void configTable()
+	{
+		//TODO
+	}
+	
+	private void fillTable()
+	{
+		//TODO
+	}
+	
 
 	public BrowserMode getMode() {
 		return mode;
@@ -69,13 +97,77 @@ public class ReactionBrowser extends JFrame
 	public void setTargetString(String targetString) {
 		this.targetString = targetString;
 	}
+	
+	public List<BrowseItem> getBrowseItems() {
+		return browseItems;
+	}
 
+	public void setBrowseItems(List<BrowseItem> browseItems) {
+		this.browseItems = browseItems;
+	}
+
+	public List<Integer> getItemSelection() {
+		return itemSelection;
+	}
+
+	public void setItemSelection(List<Integer> itemSelection) {
+		this.itemSelection = itemSelection;
+	}
+	
 	public boolean isShowRowNumber() {
 		return showRowNumber;
 	}
 
 	public void setShowRowNumber(boolean showRowNumber) {
 		this.showRowNumber = showRowNumber;
+	}
+
+	public boolean isShowReactionName() {
+		return showReactionName;
+	}
+
+	public void setShowReactionName(boolean showReactionName) {
+		this.showReactionName = showReactionName;
+	}
+
+	public boolean isShowReactionSmirks() {
+		return showReactionSmirks;
+	}
+
+	public void setShowReactionSmirks(boolean showReactionSmirks) {
+		this.showReactionSmirks = showReactionSmirks;
+	}
+
+	public boolean isShowTarget() {
+		return showTarget;
+	}
+
+	public void setShowTarget(boolean showTarget) {
+		this.showTarget = showTarget;
+	}
+
+	public boolean isShowProducts() {
+		return showProducts;
+	}
+
+	public void setShowProducts(boolean showProducts) {
+		this.showProducts = showProducts;
+	}
+
+	public boolean isShowReactionScore() {
+		return showReactionScore;
+	}
+
+	public void setShowReactionScore(boolean showReactionScore) {
+		this.showReactionScore = showReactionScore;
+	}
+
+	public boolean isShowReactionDepiction() {
+		return showReactionDepiction;
+	}
+
+	public void setShowReactionDepiction(boolean showReactionDepiction) {
+		this.showReactionDepiction = showReactionDepiction;
 	}
 
 }
