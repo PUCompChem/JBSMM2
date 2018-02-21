4e9623a93ab71f9f38f1c121eb177f4e525f7e37
Gogohero <nrggig@gmail.com>
2018-02-21 14:26:19 +0200

    Logos in AboutWindow as Table

--- src/main/java/pu/reactor/helpinfo/ReactorAboutWindow.java
+++ src/main/java/pu/reactor/helpinfo/ReactorAboutWindow.java
@@ -35,15 +35,18 @@ public class ReactorAboutWindow extends JFrame{
         Border border =  textPanel.getBorder();
         Border margin = new EmptyBorder(10,10,10,10);
         textPanel.setBorder(new CompoundBorder(margin,border));
-
+        this.add(textPanel);
         this.add(logosPanel, BorderLayout.WEST);
         this.add(textPanel, BorderLayout.CENTER);
 
 
         logosPanel.setLayout(new BoxLayout(logosPanel, BoxLayout.PAGE_AXIS));
-
-
         textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.PAGE_AXIS));
+
+
+
+
+
         Image logosImage = null;
         try {
             logosImage = ImageIO.read( ClassLoader.getSystemResource("pu/images/test.png"));
@@ -51,82 +54,86 @@ public class ReactorAboutWindow extends JFrame{
             e.printStackTrace();
         }
         JLabel label = new JLabel(new ImageIcon(logosImage));
-        logosPanel.add(label);
+      //  logosPanel.add(label);
 
 
 
 
 
-//        Icon aboutIcon = new ImageIcon("pu/images/IdeaLogo.png");
-//        Icon addIcon = new ImageIcon("pu/images/IdeaLogo.png");
-//        Icon copyIcon = new ImageIcon("pu/images/IdeaLogo.png");
-//        String[] columnNames = {"Picture", "Description"};
-//        Object[][] data =
-//                {
-//                        {aboutIcon, "About"},
-//                        {addIcon, "Add"},
-//                        {copyIcon, "Copy"},
-//                };
-//
-//        DefaultTableModel model = new DefaultTableModel(data, columnNames)
-//        {
-//            //  Returning the Class of each column will allow different
-//            //  renderers to be used based on Class
-//            public Class getColumnClass(int column)
-//            {
-//                return getValueAt(0, column).getClass();
-//            }
-//        };
-//        JTable table = new JTable( model );
-//        table.setPreferredScrollableViewportSize(table.getPreferredSize());
-//
-//        JScrollPane scrollPane = new JScrollPane( table );
-//        add( scrollPane );
-//            logosPanel.add(table);
+        Image cLabLogo = null;
+        try {
+            cLabLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/cLabLogo.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        cLabLogo = cLabLogo.getScaledInstance(150, 100, Image.SCALE_DEFAULT);
 
-//        logosPanel.setLayout(new BoxLayout(logosPanel, BoxLayout.PAGE_AXIS));
-//        textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.PAGE_AXIS));
-//        Image cLabLogo = null;
-//        try {
-//            cLabLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/cLabLogo.png"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        JLabel cLabLabel = new JLabel(new ImageIcon(cLabLogo));
-//        logosPanel.add(cLabLabel);
-//
-//
-//        Image ideaLogo = null;
-//        try {
-//            ideaLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/IdeaLogo.png"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        Image ideaLogoResized = ideaLogo.getScaledInstance(150, 100, Image.SCALE_DEFAULT);
-//        JLabel ideaLabel = new JLabel(new ImageIcon(ideaLogoResized));
-//        logosPanel.add(ideaLabel);
-//
-//
-//        Image facultyLogo = null;
-//        try {
-//            facultyLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/ChemFacultyLogo.png"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        JLabel chemFacultyLabel = new JLabel(new ImageIcon(facultyLogo));
-//        logosPanel.add(chemFacultyLabel);
-//
-//
-//        Image uniLogo = null;
-//
-//        try {
-//            uniLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/PlovdivUniversityLogo.png"));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        Image uniLogoResized = uniLogo.getScaledInstance(400, 120, Image.SCALE_DEFAULT);
-//        JLabel universityLabel = new JLabel(new ImageIcon(uniLogoResized));
-//        logosPanel.add(universityLabel);
+        Image ideaLogo = null;
+        try {
+            ideaLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/IdeaLogo.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ideaLogo = ideaLogo.getScaledInstance(150, 100, Image.SCALE_DEFAULT);
+
+
+        Image facultyLogo = null;
+        try {
+            facultyLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/ChemFacultyLogo.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        facultyLogo = facultyLogo.getScaledInstance(150, 100, Image.SCALE_DEFAULT);
+
+
+        Image uniLogo = null;
+        try {
+            uniLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/PULogo.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        uniLogo = uniLogo.getScaledInstance(150, 150, Image.SCALE_DEFAULT);
+
+
+        Icon cLabIcon = new ImageIcon(cLabLogo);
+        Icon ideaIcon = new ImageIcon(ideaLogo);
+        Icon facultyIcon = new ImageIcon(facultyLogo);
+        Icon uniIcon = new ImageIcon(uniLogo);
+
+
+        String[] columnNames = {"Picture", "Description"};
+        Object[][] data =
+                {
+                        {cLabIcon, "cLab"},
+                        {ideaIcon, "Idea Consult"},
+                        {facultyIcon, "Faculty of Chemistry"},
+                        {uniIcon, "Plovdiv University"},
+                };
+
+        DefaultTableModel model = new DefaultTableModel(data, columnNames)
+        {
+            //  Returning the Class of each column will allow different
+            //  renderers to be used based on Class
+            public Class getColumnClass(int column)
+            {
+                return getValueAt(0, column).getClass();
+            }
+        };
+        JTable table = new JTable( model );
+
+        logosPanel.add(table);
+
+        table.setRowHeight(150);
+        table.getColumnModel().getColumn(1).setWidth(100);
+        table.setBackground(new Color(238,238,238));
+
+        table.setShowGrid(false);
+        table.setShowVerticalLines(false);
+        table.setShowGrid(false);
+        table.setShowHorizontalLines(false);
+
+        table.setTableHeader(null);
+        logosPanel.setPreferredSize(new Dimension(300,300));
 
 
         JLabel name = new JLabel("Reactor");
