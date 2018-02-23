6167d4f199bdb558df430b2937a1e92d8d683633
Gogohero <nrggig@gmail.com>
2018-02-23 13:47:01 +0200

    usedLogosAsOneImage

--- src/main/java/pu/reactor/helpinfo/ReactorAboutWindow.java
+++ src/main/java/pu/reactor/helpinfo/ReactorAboutWindow.java
@@ -54,86 +54,86 @@ public class ReactorAboutWindow extends JFrame{
             e.printStackTrace();
         }
         JLabel label = new JLabel(new ImageIcon(logosImage));
-      //  logosPanel.add(label);
+       logosPanel.add(label);
 
 
 
+//
+//
+//        Image cLabLogo = null;
+//        try {
+//            cLabLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/cLabLogo.png"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        cLabLogo = cLabLogo.getScaledInstance(150, 100, Image.SCALE_DEFAULT);
+//
+//        Image ideaLogo = null;
+//        try {
+//            ideaLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/IdeaLogo.png"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        ideaLogo = ideaLogo.getScaledInstance(150, 100, Image.SCALE_DEFAULT);
+//
+//
+//        Image facultyLogo = null;
+//        try {
+//            facultyLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/ChemFacultyLogo.png"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        facultyLogo = facultyLogo.getScaledInstance(150, 100, Image.SCALE_DEFAULT);
+//
+//
+//        Image uniLogo = null;
+//        try {
+//            uniLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/PUlogo.png"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        uniLogo = uniLogo.getScaledInstance(150, 150, Image.SCALE_DEFAULT);
+//
+//
+//        Icon cLabIcon = new ImageIcon(cLabLogo);
+//        Icon ideaIcon = new ImageIcon(ideaLogo);
+//        Icon facultyIcon = new ImageIcon(facultyLogo);
+//        Icon uniIcon = new ImageIcon(uniLogo);
+//
+//
+//        String[] columnNames = {"Picture", "Description"};
+//        Object[][] data =
+//                {
+//                        {cLabIcon, "cLab"},
+//                        {ideaIcon, "Idea Consult"},
+//                        {facultyIcon, "Faculty of Chemistry"},
+//                        {uniIcon, "Plovdiv University"},
+//                };
+//
+//        DefaultTableModel model = new DefaultTableModel(data, columnNames)
+//        {
+//            //  Returning the Class of each column will allow different
+//            //  renderers to be used based on Class
+//            public Class getColumnClass(int column)
+//            {
+//                return getValueAt(0, column).getClass();
+//            }
+//        };
+//        JTable table = new JTable( model );
+//
+//        logosPanel.add(table);
 
-
-        Image cLabLogo = null;
-        try {
-            cLabLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/cLabLogo.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        cLabLogo = cLabLogo.getScaledInstance(150, 100, Image.SCALE_DEFAULT);
-
-        Image ideaLogo = null;
-        try {
-            ideaLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/IdeaLogo.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        ideaLogo = ideaLogo.getScaledInstance(150, 100, Image.SCALE_DEFAULT);
-
-
-        Image facultyLogo = null;
-        try {
-            facultyLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/ChemFacultyLogo.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        facultyLogo = facultyLogo.getScaledInstance(150, 100, Image.SCALE_DEFAULT);
-
-
-        Image uniLogo = null;
-        try {
-            uniLogo = ImageIO.read( ClassLoader.getSystemResource("pu/images/PUlogo.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        uniLogo = uniLogo.getScaledInstance(150, 150, Image.SCALE_DEFAULT);
-
-
-        Icon cLabIcon = new ImageIcon(cLabLogo);
-        Icon ideaIcon = new ImageIcon(ideaLogo);
-        Icon facultyIcon = new ImageIcon(facultyLogo);
-        Icon uniIcon = new ImageIcon(uniLogo);
-
-
-        String[] columnNames = {"Picture", "Description"};
-        Object[][] data =
-                {
-                        {cLabIcon, "cLab"},
-                        {ideaIcon, "Idea Consult"},
-                        {facultyIcon, "Faculty of Chemistry"},
-                        {uniIcon, "Plovdiv University"},
-                };
-
-        DefaultTableModel model = new DefaultTableModel(data, columnNames)
-        {
-            //  Returning the Class of each column will allow different
-            //  renderers to be used based on Class
-            public Class getColumnClass(int column)
-            {
-                return getValueAt(0, column).getClass();
-            }
-        };
-        JTable table = new JTable( model );
-
-        logosPanel.add(table);
-
-        table.setRowHeight(150);
-        table.getColumnModel().getColumn(1).setWidth(100);
-        table.setBackground(new Color(238,238,238));
-
-        table.setShowGrid(false);
-        table.setShowVerticalLines(false);
-        table.setShowGrid(false);
-        table.setShowHorizontalLines(false);
-
-        table.setTableHeader(null);
-        logosPanel.setPreferredSize(new Dimension(300,300));
+//        table.setRowHeight(150);
+//        table.getColumnModel().getColumn(1).setWidth(100);
+//        table.setBackground(new Color(238,238,238));
+//
+//        table.setShowGrid(false);
+//        table.setShowVerticalLines(false);
+//        table.setShowGrid(false);
+//        table.setShowHorizontalLines(false);
+//
+//        table.setTableHeader(null);
+        logosPanel.setPreferredSize(new Dimension(400,300));
 
 
         JLabel name = new JLabel("Reactor");
