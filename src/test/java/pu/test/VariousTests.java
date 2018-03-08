5c8077860a233e2a3f901eb8d96f1f58b0db62fc
ntk73 <nick@uni-plovdiv.net>
2018-03-08 21:30:07 +0200

    small change

--- src/test/java/pu/test/VariousTests.java
+++ src/test/java/pu/test/VariousTests.java
@@ -64,7 +64,7 @@ public class VariousTests
 		
 		//testReadWorkspaceJson("/test-workspace.json");
 		
-		testReadPreferencesJson("/test-preferences.json");
+		testReadPreferencesJson("/test-preferences.json" );
 		
 		//testResources();
 		
