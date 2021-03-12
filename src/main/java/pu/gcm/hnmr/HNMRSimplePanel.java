1462fce766d5b2c407a78ac31658d5ee2fd1f5ea
ntk73 <nick@uni-plovdiv.net>
2021-03-12 16:11:29 +0200

    Added new package pu.gcm.hnmr and initial versions of GUI classes: AmbitHnmrGUI and HNMRSimplePanel

---
+++ src/main/java/pu/gcm/hnmr/HNMRSimplePanel.java
@@ -0,0 +1,74 @@
+package pu.gcm.hnmr;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+import ambit2.smarts.SmartsHelper;
+import ambit2.ui.Panel2D;
+import pu.gui.utils.chemtable.StructureTable;
+
+public class HNMRSimplePanel extends JPanel 
+{
+
+	//GUI components
+	JPanel leftArea, rightArea;
+	JPanel targetPanel, matchPanel, reactionPanel, buttonsPanel, configPanel;
+	JTextField smilesField;
+	JTextArea resultTextArea;
+	Panel2D panel2d;	
+	JButton applyButton;
+	
+	//Chem data
+	IAtomContainer targetMol = null;
+	
+	
+	public HNMRSimplePanel() throws Exception
+	{
+		initGUI();
+
+		//Setup initial demo chem data
+		try {
+			targetMol = SmartsHelper.getMoleculeFromSmiles("c1ccncc1");
+			panel2d.setAtomContainer(targetMol);
+		} catch (Exception e) {}
+		smilesField.setText("CCCC");
+		//smirksField.setText("[c:1][H]>>[c:1]O[H]");
+		//panel2dMatch.setAtomContainer(targetMol.clone());
+	}
+	
+	
+	private void initGUI()
+	{	
+		this.setLayout(new BorderLayout());
+		leftArea = new JPanel(new BorderLayout());
+		rightArea = new JPanel(new BorderLayout());
+		this.add(leftArea, BorderLayout.CENTER);
+		this.add(rightArea, BorderLayout.EAST);
+		
+		//Target panel
+		targetPanel = new JPanel(new BorderLayout());
+		//this.add(targetPanel, BorderLayout.NORTH);
+		leftArea.add(targetPanel, BorderLayout.NORTH);
+		targetPanel.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+		JPanel smilesPanel = new JPanel(new FlowLayout());
+		JLabel smilesInputLabel = new JLabel("Target molecule (Smiles/InChI): ");
+		smilesField = new JTextField(30);
+		smilesPanel.add(smilesInputLabel);
+		smilesPanel.add(smilesField);		
+		targetPanel.add(smilesPanel, BorderLayout.NORTH);
+		panel2d = new Panel2D();
+		panel2d.setPreferredSize(new Dimension(150,150));
+		targetPanel.add(panel2d, BorderLayout.CENTER);
+	}
+}
